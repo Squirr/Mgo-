9b546565d68284f2d9436921d75bae6b7a4a84f5
Squirr <87684950+Squirr@users.noreply.github.com>
2024-05-17 16:47:22 +0800

    初始化
    项目创建

---
+++ src/main/java/service/mgo/mgo/MgoApplication.java
@@ -0,0 +1,13 @@
+package service.mgo.mgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MgoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MgoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/service/mgo/mgo/MgoApplicationTests.java
@@ -0,0 +1,13 @@
+package service.mgo.mgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MgoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
